ae9fd327d8d9546efe004e1ad4e44e909e9c6590
Nishanth <nishanthkiran2@gmail.com>
2024-05-29 23:30:50 +0530

    Updates to Font Sizes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 export default function Nav() {
     return(
-        <nav onTouchStart={()=>window.open("/", "_self")} className="font-bold font-mono bg-red-200 shadow-md p-3 border-b-2 border-b-black text-3xl">
-            <p className="inline-block transform-origin animate-pulse text-5xl">Q</p>uizzer
+        <nav onTouchStart={()=>window.open("/", "_self")} className="font-bold font-mono bg-red-200 shadow-md p-3 border-b-2 border-b-black text-2xl">
+            <p className="inline-block transform-origin animate-pulse text-3xl">Q</p>uizzer
         </nav>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     const navigate = useNavigate();
 
     return(
-        <div className="font-mono bg-red-100 h-screen p-4 flex text-3xl justify-center items-center flex-col">
+        <div className="font-mono bg-red-100 h-screen p-4 flex text-xl justify-center items-center flex-col">
             <p className="font-normal italic text-center"><b>Note: </b>You will have 30s to answer each question. And, there will be 20 questions in total.</p>
             <button onClick={()=>navigate("/quiz")} className="mt-14 mb-6 p-4 bg-red-200 rounded-lg font-semibold shadow-md touch:bg-pink-200 touch:text-gray-500 touch:shadow-none touch:translate-y-1 animate-bounce">Start!</button>
             <p className="font-bold">All the Best!</p>
